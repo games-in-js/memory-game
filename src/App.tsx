f19f0de9b3fa6d153aacafef3f75a70cdcef035c
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2024-12-28 20:39:10 -0300

    chore: create useMemoryGame hook to save cards state

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import GameBoard from "@/components/GameBoard";
 import ScoreBoard from "@/components/ScoreBoard";
 import GameModal from "@/components/GameModal";
 import DifficultySelector from "@/components/DifficultySelector";
+import { useMemoryGame } from "@/hooks/use-memory-game";
 
 function App() {
   const gameCompleted = false;
   const difficulty = "easy";
+
+  const { cards } = useMemoryGame();
+
+  console.log(cards);
 
   if (!difficulty) return <DifficultySelector />;
 
@@ -13,7 +18,7 @@ function App() {
     <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4 bg-blue-200 p-4 sm:gap-8 sm:p-8">
       <ScoreBoard moves={0} time={0} onRestart={() => {}} />
 
-      <GameBoard />
+      <GameBoard cards={cards} />
 
       {gameCompleted && <GameModal moves={10} time={0} onRestart={() => {}} />}
     </div>
--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,25 +1,19 @@
 import { motion } from "motion/react";
-import Card from "./Card";
 import { ANIMATIONS } from "@/constants";
+import { Card as CardType } from "@/types";
+import Card from "./Card";
 
-const EMOJIS = ["🍭", "🍬", "🍫", "🧁", "🍪", "🍩"] as const;
+type GameBoardProps = {
+  cards: CardType[];
+};
 
-const CARDS = [...EMOJIS, ...EMOJIS]
-  .sort(() => Math.random() - 0.5)
-  .map((emoji, index) => ({
-    id: index,
-    emoji,
-    isFlipped: false,
-    isMatched: false,
-  }));
-
-function GameBoard() {
+function GameBoard({ cards }: GameBoardProps) {
   return (
     <motion.div
       {...ANIMATIONS.fadeInUp}
       className="grid grid-cols-4 gap-2 rounded-xl bg-blue-100 p-2 sm:gap-4 sm:p-4"
     >
-      {CARDS.map((card) => (
+      {cards.map((card) => (
         <Card key={card.id} {...card} />
       ))}
     </motion.div>
