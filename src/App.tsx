c518bbd6ff045507a27865466606b5845092cdfc
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2024-12-28 13:12:59 -0300

    chore: add ScoreBoard

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import GameBoard from "@/components/GameBoard";
+import ScoreBoard from "@/components/ScoreBoard";
 
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-blue-200">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4 bg-blue-200 p-4">
+      <ScoreBoard moves={0} time={0} onRestart={() => {}} />
+
       <GameBoard />
     </div>
   );
